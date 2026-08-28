4f70a2aae25d5655f47713f94be3161bf3739d0c
ilragno227 <ilragno227@gmail.com>
2026-08-28 11:58:54 +0300

    Edited App Name

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def detect_language(text):
 
 
 # 3. Streamlit Web UI Layout
-st.title("🌐 Language Detection App")
+st.title("🌐 Ilragnino's Language Detection")
 st.write("Enter text below to detect its language:")
 
 # Input text area
